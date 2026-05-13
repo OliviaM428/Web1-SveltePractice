08905e1557da19b7e1bd29af1fed30e0414562c0
MILTOL1 <MILTOL1@D207-78657>
2026-05-13 14:38:35 +1200

    add html

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
+<header>
+    <h1>Welcome to a {title} SvelteKit</h1>
+</header>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>Welcome to a website</p>
+<img src = "https://www.nbcstore.com/cdn/shop/products/SHREK-SS-63-MF1_576x.jpg?v=1693905182" alt="Shrek">
+
 
 <style>
-    p{
+    h1{
         color: red;
+        background-color: black;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        font-size: 2.5em;
     }
+
+    p{
+        color: green;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+    }
+    
+    img{
+        border: solid 15px greenyellow;
+        border-radius: 3%;
+    }
+
+
 </style>
+<script>
+    let title = "super cool";
+</script>
