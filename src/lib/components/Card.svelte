e816f58e3af4402a7ef329fd1f8528967ac0da14
MILTOL1 <MILTOL1@D207-78657>
2026-05-15 10:37:13 +1200

    add componet folder. Card Svelte

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,12 @@
+<script>
+    let name = "Web";
+</script>
+
+<p>{name} Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum porro accusantium maxime perspiciatis eos animi ratione molestiae sapiente adipisci est? Explicabo doloremque mollitia labore omnis corporis dolorum eos error corrupti.</p>
+
+<style>
+    p{
+        color:hotpink;
+    }   
+
+</style>
