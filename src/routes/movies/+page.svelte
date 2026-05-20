7beedc39c24ecea2483a51577c34e001bb635ada
MILTOL1 <MILTOL1@D207-78657>
2026-05-20 14:47:17 +1200

    added route to movies folder

---
+++ src/routes/movies/+page.svelte
@@ -0,0 +1,24 @@
+<h2>Any old movie</h2>
+
+<p>data for movie goes here</p>
+
+
+
+<div class="movie">
+    <img src={image} alt="Placeholder Movie">   
+</div>
+
+<style>
+    .movie {   
+        display: flex;
+        gap: 1em;
+        border: 1px solid #ddd;
+        margin-bottom: 1em;
+        padding: 1em;
+        max-width: 400px;
+    }   
+
+    .movie img{
+        max-width: 200px;
+    }
+</style>
---
+++ src/routes/movies/[bananas]/+page.svelte
@@ -0,0 +1,19 @@
+<h2>Movies Options</h2>
+
+<nav>
+    <ul>
+        <li><a href="/movies/indy">Indian Jones</a></li>
+        <li><a href="/movies/star-wars">Star Wars</a></li>
+        <li><a href="/movies/fugitive">The Fugitive</a></li>
+    </ul>
+</nav>
+
+<style>
+    li{
+        list-style-type: none;
+        font-size: 1.5em;
+    }
+    ul{
+        padding-left: 0;
+    }
+</style>
