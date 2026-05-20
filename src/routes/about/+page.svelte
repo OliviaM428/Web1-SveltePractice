965e03e6c1b3331edf9765d9479478979d2be0d4
MILTOL1 <MILTOL1@D207-78657>
2026-05-20 14:06:16 +1200

    created and linked about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,27 @@
+<nav class ="navbar">
+    <a href ="/">Home</a>
+    <a href ="/about">About</a>
+</nav>
+
+<h1>About Page</h1>
+
+<p>welcome to the about page</p>
+
+<img src="https://upload.wikimedia.org/wikipedia/en/thumb/b/bc/Garfield_the_Cat.svg/250px-Garfield_the_Cat.svg.png" alt="Garfield">
+
+<style>
+    .navbar{
+        background-color: rgb(92, 92, 92);
+    }
+    
+    .navbar a{
+        color: white;
+        font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; 
+        padding:  14px 16px;   
+    }
+
+    .navbar a:hover{
+        background-color: rgb(43, 43, 43);
+    }
+
+</style>
